6d190a68fe5fb85399967aea9c37443d435329bd
gio <gioele.burgarello99@gmail.com>
2026-05-24 20:58:25 +0200

    Update autostart commands in hyprland.lua
    Removed commented autostart commands and retained only the waybar command.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -45,11 +45,12 @@ local menu        = "fuzzel"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function()
+  --   hl.exec_cmd(terminal)
+  --   hl.exec_cmd("nm-applet")
+  --   hl.exec_cmd("waybar & hyprpaper & firefox")
+  hl.exec_cmd("waybar")
+end)
 
 
 -------------------------------
