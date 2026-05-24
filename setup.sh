5df0ecad216900f3e439c119e5b9dcbd88984a6a
gio <gioele.burgarello99@gmail.com>
2026-05-24 17:00:31 +0200

    Enhance setup script for environment installation
    Added commands to enable PipeWire services and install Yay.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@ echo "i will install various packages for the envirronment"
 
 # audio things
 sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils pavucontrol
+systemctl --user enable --now pipewire wireplumber pipewire-pulse
 
 # base things for the hyprland envirronment
 sudo pacman -S hyprland fuzzel alacritty firefox
 
 # yay
+cd
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 # font
