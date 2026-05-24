0093b41e44e7308db2a758c145a465599aa42263
gio <gioele.burgarello99@gmail.com>
2026-05-24 16:37:10 +0200

    Add setup script for environment package installation

---
+++ setup.sh
@@ -0,0 +1,25 @@
+echo "i will install various packages for the envirronment"
+
+# audio things
+sudo pacman -S pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils pavucontrol
+
+# base things for the hyprland envirronment
+sudo pacman -S hyprland fuzzel alacritty firefox
+
+# yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+# font
+sudo pacman -S ttf-jetbrains-mono
+
+# clipboard
+sudo pacman -S wl-clipboard
+
+# neovim setup
+sudo pacman -S nvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+# hyprland setup
+mkdir ~/.config
+cd ~/HyprDots/.config && cp -r . ~/.config/
